4e52ecd67276f05a5340b716bda69f56fc75c2ca
Microwave <121560464+Micro856@users.noreply.github.com>
2025-08-18 14:12:20 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -27,11 +27,9 @@ mkdir -p /var/cache/lightdm
 mkdir -p /var/lib/lightdm-data
 mkdir -p /usr/lib/sysusers.d
 
-echo "g lightdm" > | \
-          systemd-sysusers --replace=/usr/lib/sysusers.d/lightdm-group.conf -
+echo "g lightdm" | systemd-sysusers --replace=/usr/lib/sysusers.d/lightdm-group.conf -
 
-echo "u lightdm - "Light Display Manager" /var/lib/lightdm" | \
-          systemd-sysusers --replace=/usr/lib/sysusers.d/lightdm.conf -
+echo "u lightdm - "Light Display Manager" /var/lib/lightdm" | systemd-sysusers --replace=/usr/lib/sysusers.d/lightdm.conf -
 
 sudo rm -rf /var/run
 sudo ln -s /run /var/run
